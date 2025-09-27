b00a828a184accd93fe65f626727247dd4ab7282
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-27 15:50:22 +0530

    roboshop project commits

---
+++ frontend.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#colour codes
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+B="\e[34m"
+
+SCRIPT_DIR=$PWD
+START_TIME=$(date +%s)
+LOG_DIR=/var/log/shell_roboshop_project
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE=${LOG_DIR}/${SCRIPT_NAME}.log
+mkdir -p $LOG_DIR
+
+#To check root user or not
+ROOT_USER=$(id -u)
+if [ $ROOT_USER -ne 0 ]; then
+    echo "Pease run the script under root privilages"
+    exit 1
+fi
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]; then
+        echo -e "$2...$R FAILURE $N"
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+dnf module disable nginx -y &>>$LOG_FILE
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+systemctl start nginx 
+VALIDATE $? "start Nginx"
+
+rm -rf /usr/share/nginx/html/* 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Downloading frontend"
+
+rm -rf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx.conf"
+
+systemctl restart nginx 
+VALIDATE $? "Restarting Nginx"
+
+
