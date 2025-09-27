b6cd8c4bfc72447b5bf8eaf13daf0eac010eba7e
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-27 16:46:50 +0530

    roboshop project commits

--- catalogue.sh
+++ catalogue.sh
@@ -79,9 +79,9 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
 
 
-INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh $MONGODB_SERVER_IPADDRESS --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -ne 0 ]; then
-    mongosh --host $MONGODB_SERVER_IPADDRESS </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host $c </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
 else
     echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
