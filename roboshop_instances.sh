8d6c24a8d9680361ef954c4c41747a4266832f43
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-27 13:14:53 +0530

    roboshop project commits

--- roboshop_instances.sh
+++ roboshop_instances.sh
@@ -5,7 +5,7 @@ SG_ID="sg-0dfa2ec6d3272ffeb"
 
 
 
-for INSTANCE in #@; do
+for INSTANCE in $@; do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" --query 'Instances[0].InstanceId' --output text)
     if [ $INSTANCE_ID -ne "frontend" ]; then
         #get privateip
