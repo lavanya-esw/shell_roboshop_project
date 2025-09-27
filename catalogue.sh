3cb2a907c272d164439167ad9af8edb552523374
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-27 16:42:26 +0530

    roboshop project commits

--- catalogue.sh
+++ catalogue.sh
@@ -47,7 +47,7 @@ if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
     VALIDATE $? "Creating system user"
 else
-    echo "roboshop user is already created.....$Y SKIPPING $N"
+    echo -e "roboshop user is already created.....$Y SKIPPING $N"
 fi    
 
 mkdir -p /app 
